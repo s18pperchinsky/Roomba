2f7d6594e161152e4a009d2539f1b9eeb96da452
paige perchinsky <s18pperchinsky@severnschool.com>
2017-09-26 13:21:17 -0400

    Fixed Paranthesis

--- Roomba.c
+++ Roomba.c
@@ -8,18 +8,19 @@ task main()
 	int speed = 10;
 	int counter = 1000;
  while (true)
- {
+{
 		motor (motorB) = 82;
  	 	motor (motorC) = 100;
- 	  if (sensorvalue (buttonleft) == 1)
- 	  {
+}
+ 	 	if (sensorvalue (buttonleft) == 1)
+{
  	  	motor (motorB) = -100;
  	  	motor (motorC) = -100;
  	  	wait1Msec (550);
  	  	motor (motorB) = -50;
  	  	motor (motorC) = 50;
  	  	wait1Msec (500);
- 		}
+}
  		if (sensorvalue (buttonright) == 1)
  	  {
  	  	motor (motorB) = -100;
@@ -44,8 +45,31 @@ task main()
 	 		wait1Msec (1000);
 		}
 
+task main ()
+{
+	int speed = 10;
+	int counter = 1000;
+ while (true)
 
+		motor (motorB) = 82;
+}
+		task main()
+ 	  	motor (motorC) = 50;
+ 	  	wait1Msec (500);
+ 		}
+ 		while (counter <= 1000)
+		{
+	   motor (motorC) = 100;
+	   motor (motorB) = speed;
+	   wait1Msec (200);
+	   speed = speed + 1;
+	   counter = counter -10;
+		}
+		if (speed == 100)
+		{
+	 		motor (motorB) = 100;
+	 		motor (motorC) = 100;
+	 		wait1Msec (1000);
+		}
   }
-
-
 }
